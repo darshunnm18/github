fdfaea71fd7fe0c7b6295bbb956f2bfd2e57974d
darshunnm18 <darshannm222@gmail.com>
2025-07-10 16:34:21 +0530

    static_variable

---
+++ static.py
@@ -0,0 +1,14 @@
+class Farmer:
+    r=2.5
+    def __init__(self,p1,t1):
+        self.p=p1
+        self.t=t1
+    def disp(self):
+        SI=(self.p*self.t*Farmer.r)/100
+        print(SI)
+f1=Farmer(100000,2)
+f2=Farmer(200000,3)
+f3=Farmer(500000,5)
+f1.disp()
+f2.disp()
+f3.disp()
