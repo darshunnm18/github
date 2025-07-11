c73c72a057d38c6461bdfaade920f6ac92dc82fe
darshunnm18 <darshannm222@gmail.com>
2025-07-11 15:36:08 +0530

    example_py

--- README.md
+++
@@ -1,2 +0,0 @@
-# My first GitHub Project
-This is my first GitHub project. I published this project directly from visual studio code.
---
+++ example.py
@@ -0,0 +1,21 @@
+a=10
+b=10
+c=10
+print(a)
+print(b)
+print(c)
+print(id(a))
+print(id(b))
+print(id(c))
+print(a is b)
+d=20
+e=20
+f=20
+print(d)
+print(e)
+print(f)
+print(id(d))
+print(id(e))
+print(id(f))
+print(a is d)
+print(f is d)
