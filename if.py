f0935e3b8e3269cb89316d6243cc59a7641aea58
darshunnm18 <darshannm222@gmail.com>
2025-07-14 16:36:48 +0530

    if_py

---
+++ if.py
@@ -0,0 +1,12 @@
+age=int(input("enter age"))
+if age>=18:
+    print("you can vote")
+    print("you-21 are a citizen")
+if 21>=18:
+    print("you can vote")
+    print("you are a citizen")   
+    age=-21
+if-21>=18:
+    print("you are a citizen")
+
+    
---
+++ string.py
@@ -0,0 +1,12 @@
+s1="abbc123"
+print("a" in s1)
+print("bbc" in s1)
+print("12" in s1)
+print("23" in s1)
+
+print("===============")
+a="10"
+print("1" in a)
+print("===============")
+a=""
+print("ab" in a)
