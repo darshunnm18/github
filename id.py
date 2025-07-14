e209eb2690081e8440f3eec30bcbe0897c038c83
darshunnm18 <darshannm222@gmail.com>
2025-07-14 15:40:04 +0530

    id_py

--- id.py
+++ id.py
@@ -1,3 +1,30 @@
-s1="abc"
-print(s1)
-print(id(s1))
+a=100
+b=100
+c=100
+d=a
+print(a==b==c==d)
+#same memory is shared 
+#same output
+print(a is b)
+print(b is c)
+print(a is c)
+print("==========")
+x=10
+print(x)
+y=x
+print(y)
+print(x is y)
+x=20
+print(x)
+print(x is y)
+print("==========")
+x=10
+print(x)
+print(id(x))
+x=20
+print(x)
+print(id(x))
+x=30
+print(x)
+print(id(x))
+      
