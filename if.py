fcbe7b197142c234c4166e0dfe5440e2c6b0c829
darshunnm18 <darshannm222@gmail.com>
2025-07-14 16:55:51 +0530

    ifelse_py

--- if.py
+++ if.py
@@ -1,12 +1,8 @@
-age=int(input("enter age"))
-if age>=18:
-    print("you can vote")
-    print("you-21 are a citizen")
-if 21>=18:
-    print("you can vote")
-    print("you are a citizen")   
-    age=-21
-if-21>=18:
-    print("you are a citizen")
-
-    
+num=int(input("enter num:"))
+if num%2==0:
+    print(f"{num} is even")
+else:
+    print(f"{num} is odd")
+print("prog exec")
+   
+   
