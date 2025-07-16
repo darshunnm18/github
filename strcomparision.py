9176327da8ce3acbc7abd824f2ebc965fe6346eb
darshunnm18 <darshannm222@gmail.com>
2025-07-16 16:04:29 +0530

    strcomparision_py

---
+++ strcomparision.py
@@ -0,0 +1,13 @@
+# comparision string which is greater
+print("enter a string1")# input ramu
+str1=input()
+print("enter a string2")# input rama
+str2=input()
+if(str1>str2):
+    print("str1 is grt")
+elif(str2>str1):
+    print("str2 is grt")
+else:
+    print("str1=str2")
+    # output str1 is greater  
+    
