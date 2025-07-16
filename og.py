78ee86363849e500171108319901f0e2338dcb0c
darshunnm18 <darshannm222@gmail.com>
2025-07-16 16:33:25 +0530

    og_py

---
+++ og.py
@@ -0,0 +1,9 @@
+#format keyvalue is using
+print("enter a name")
+name=input()
+print("name={}".format(name))
+print("enter a age")
+age=input()
+print("enter a addr")
+addr=input()
+print("name={}\n age={}\n addr={}".format(name,age,addr))
---
+++ substringinmainstru=ing.py
@@ -0,0 +1,12 @@
+# finding substring in given string
+str="If you think you can or you can't,you are right"
+print(str)
+print(str.count("you"))
+str1="you"
+print(str.find("you"))
+print(str1 in str)
+print(str.index("you"))
+print(str.rfind("you"))
+print(str.rindex("you"))
+print(str.find("kohli"))
+print(str.index("kohli"))
